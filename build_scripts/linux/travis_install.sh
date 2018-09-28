a3cec4dc7c8e417dd2759aee6e1780e10262e283
Jan Noha <nohajc@gmail.com>
2018-09-28 15:14:25 +0200

    Travis: don't use add-apt-repository, do just one apt update

--- build_scripts/linux/travis_install.sh
+++ build_scripts/linux/travis_install.sh
@@ -2,13 +2,12 @@
 
 echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main' | sudo tee -a /etc/apt/sources.list
 echo 'deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://ppa.launchpad.net/george-edison55/cmake-3.x/ubuntu trusty main' | sudo tee -a /etc/apt/sources.list
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AF4F7421
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA9EF27F
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 828AB726
 
-sudo apt update
-sudo apt install software-properties-common -y
-sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt update
 
 sudo apt install zlib1g-dev cmake3 gcc-8 g++-8 llvm-6.0-dev libclang-6.0-dev -y
