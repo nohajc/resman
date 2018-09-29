d9a483c237e49f029977caca4eb8967e4f0c7319
Jan Noha <nohajc@gmail.com>
2018-09-30 00:21:18 +0200

    Actually, just create the build directory outside of the container

--- build_scripts/linux/travis_script.sh
+++ build_scripts/linux/travis_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run --rm -u $(stat -c "%u:%g" .) -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman && mkdir -p build && cd build && cmake -DLINKING=static ../rescomp && make -j4"
+mkdir -p build
+chmod 777 build
+docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4"
 
 #mkdir build && cd build
 #CC=gcc-8 CXX=g++-8 cmake ../rescomp
