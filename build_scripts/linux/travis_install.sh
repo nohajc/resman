14cf4a45249748671b8f75d3ef9fd0351777b180
Jan Noha <nohajc@gmail.com>
2018-09-30 08:58:49 +0200

    Docker build on Travis works quite nicely
    It is also faster. Remove the old script that was already commented out.

--- build_scripts/linux/travis_install.sh
+++ build_scripts/linux/travis_install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
 docker pull nohajc/void-llvm-clang-dev
-
-#echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main' | sudo tee -a /etc/apt/sources.list
-#echo 'deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main' | sudo tee -a /etc/apt/sources.list
-#echo 'deb http://ppa.launchpad.net/george-edison55/cmake-3.x/ubuntu trusty main' | sudo tee -a /etc/apt/sources.list
-#
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AF4F7421
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA9EF27F
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 828AB726
-#
-#sudo apt update -qq
-#
-#sudo apt install zlib1g-dev cmake3 gcc-8 g++-8 llvm-6.0-dev libclang-6.0-dev -y
--- build_scripts/linux/travis_script.sh
+++ build_scripts/linux/travis_script.sh
@@ -3,7 +3,3 @@
 mkdir -p build
 chmod 777 build
 docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4"
-
-#mkdir build && cd build
-#CC=gcc-8 CXX=g++-8 cmake ../rescomp
-#make -j4
