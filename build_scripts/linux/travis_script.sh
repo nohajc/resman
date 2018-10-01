9b05bcb179de5f066efacff0bc659df90d0d1f7e
Jan Noha <nohajc@gmail.com>
2018-10-01 23:12:40 +0200

    Automate linux releases

--- build_scripts/linux/travis_script.sh
+++ build_scripts/linux/travis_script.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build
 chmod 777 build
-docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4"
+docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4 && make package"
