59626a00da73d51c8b8b31d8f9a803ddb3a0d786
Jan Noha <nohajc@gmail.com>
2018-10-02 22:05:31 +0200

    Travis: make package only on tag builds

--- build_scripts/linux/travis_script.sh
+++ build_scripts/linux/travis_script.sh
@@ -2,4 +2,11 @@
 
 mkdir -p build
 chmod 777 build
-docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4 && make package"
+
+DOCKER_COMMAND="cd /home/dev/resman/build && cmake -DLINKING=static ../rescomp && make -j4"
+
+if [ -n "$TRAVIS_TAG" ]; then
+   DOCKER_COMMAND="$DOCKER_COMMAND && make package"
+fi
+
+docker run --rm -v $PWD:/home/dev/resman -it nohajc/void-llvm-clang-dev /bin/bash -c "$DOCKER_COMMAND"
--- build_scripts/osx/travis_script.sh
+++ build_scripts/osx/travis_script.sh
@@ -3,4 +3,7 @@
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=/tmp/clang-llvm-libs ../rescomp
 make -j4
-make package
+
+if [ -n "$TRAVIS_TAG" ]; then
+   make package
+fi
