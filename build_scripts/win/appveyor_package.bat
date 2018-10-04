1603219ac7ed5d5b49f07a41c75d9fcedfdd6c3b
Jan Noha <nohajc@gmail.com>
2018-10-04 21:29:28 +0200

    Funny typo in a bat file.

--- build_scripts/win/appveyor_package.bat
+++ build_scripts/win/appveyor_package.bat
@@ -1,4 +1,4 @@
-echo @off
+@echo off
 
 IF "%APPVEYOR_REPO_TAG%"=="true" (
    7z a resman-%APPVEYOR_REPO_TAG_NAME%-win-amd64.zip %APPVEYOR_BUILD_FOLDER%\rescomp\x64\Release\rescomp.exe include\resman.h
